deed8520b10479bb544eb90610d0030a48fee248
Poul <you@example.com>
2017-10-31 00:35:47 +0400

    created validator for xml

---
+++ src/main/java/com/epam/training/task8/Util.java
@@ -0,0 +1,31 @@
+package com.epam.training.task8;
+
+import com.epam.training.task7.data.*;
+import org.xml.sax.SAXException;
+
+import javax.xml.XMLConstants;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Util {
+
+    public static boolean validateXmlWithXsd(File dataFile, File schemaFile) throws SAXException, IOException {
+        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        Schema schema = schemaFactory.newSchema(schemaFile);
+        Validator validator = schema.newValidator();
+        validator.validate(new StreamSource(new FileInputStream(dataFile)));
+        return true;
+    }
+
+
+}
