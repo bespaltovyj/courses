d7e15e052fb1034c5478c57f9b63122e965fe3e3
Poul <you@example.com>
2017-11-02 02:22:20 +0400

    added tests for DFS and BFS

---
+++ src/test/java/com/epam/training/task6/NodeTest.java
@@ -0,0 +1,71 @@
+package com.epam.training.task6;
+
+import com.epam.training.task6.traversing.BreadthFirstTraversing;
+import com.epam.training.task6.traversing.DepthFirstTraversing;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NodeTest {
+    Node parentNode;
+
+    /** This method creates such a tree:
+     *                      1
+     *             2       3     4     8
+     *          5    6   7
+     *BFS: 1,2,3,4,8,5,6,7
+     *DFT: 1,2,5,6,3,7,4,8
+     */
+    @BeforeEach
+    void setUp() {
+        parentNode = new Node(1);
+
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+        Node node8 = new Node(8);
+
+        parentNode.addDescendants(node2);
+        parentNode.addDescendants(node3);
+        parentNode.addDescendants(node4);
+        parentNode.addDescendants(node8);
+
+        Node node5 = new Node(5);
+        Node node6 = new Node(6);
+        Node node7 = new Node(7);
+
+        node2.addDescendants(node5);
+        node2.addDescendants(node6);
+
+        node3.addDescendants(node7);
+    }
+
+
+    @Test
+    void testDepthFirstTraversing() {
+        parentNode.setTraversingTree(new DepthFirstTraversing());
+        List<Integer> correctResult = Arrays.asList(1, 2, 5, 6, 3, 7, 4, 8);
+        List<Integer> currentResult = new ArrayList<>();
+        for (Node node : parentNode) {
+            currentResult.add(node.getValue());
+        }
+        assertEquals(correctResult, currentResult);
+    }
+
+    @Test
+    void testBreadthFirstTraversing() {
+        parentNode.setTraversingTree(new BreadthFirstTraversing());
+        List<Integer> correctResult = Arrays.asList(1, 2, 3, 4, 8, 5, 6, 7);
+        List<Integer> currentResult = new ArrayList<>();
+        for (Node node : parentNode) {
+            currentResult.add(node.getValue());
+        }
+        assertEquals(correctResult, currentResult);
+    }
+
+}
