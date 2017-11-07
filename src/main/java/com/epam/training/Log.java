4c17a52d1be580c52ca1b4b6150df96d136419fc
Poul <you@example.com>
2017-11-07 21:39:22 +0400

    created log class

---
+++ src/main/java/com/epam/training/Log.java
@@ -0,0 +1,8 @@
+package com.epam.training;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Log {
+    public static Logger log = LogManager.getLogger("traceLogger");
+}
