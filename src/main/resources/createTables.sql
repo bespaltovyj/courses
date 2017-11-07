701afc5b788636769c6e3d7e8e8c9b0bc5e0fbf5
Poul <you@example.com>
2017-11-07 21:06:55 +0400

    created rdb.properties an sql-script

---
+++ src/main/resources/createTables.sql
@@ -0,0 +1,26 @@
+DROP TABLE Author,Book,Author_Book,Publisher,Book_Publisher;
+CREATE TABLE Author(
+  id VARCHAR PRIMARY KEY
+  ,name VARCHAR(20)
+  ,dateOfBirth DATE
+  ,dateOfDeath DATE
+  ,gender VARCHAR(6)
+);
+CREATE TABLE Book(
+  id VARCHAR PRIMARY KEY
+  ,name VARCHAR(20)
+  ,dateOfRelease DATE
+);
+CREATE TABLE Author_Book(
+  authorId VARCHAR
+  ,bookId VARCHAR
+  ,PRIMARY KEY (authorId,bookId)
+);
+CREATE TABLE Publisher(
+  id VARCHAR PRIMARY KEY
+);
+CREATE TABLE Book_Publisher(
+  bookId VARCHAR
+  ,publisherId VARCHAR
+  ,PRIMARY KEY (bookId,publisherId)
+);
