59c9efd803238de38ce5b34c95759d58e313212b
Poul <you@example.com>
2017-11-07 21:59:09 +0400

    updated Log class

--- src/main/java/com/epam/training/Log.java
+++ src/main/java/com/epam/training/Log.java
@@ -4,5 +4,5 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Log {
-    public static Logger log = LogManager.getLogger("traceLogger");
+    public static Logger log = LogManager.getRootLogger();
 }
